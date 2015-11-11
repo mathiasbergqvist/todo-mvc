3e3561213bdd62da639d88bb631dea05704feffd
mathiasbergqvist <mathiasbergqvist88@gmail.com>
2015-11-11 21:41:58 +0100

    new model for todo. Setup of mock server using ember-cli-mirage

--- app/routes/todos.js
+++ app/routes/todos.js
@@ -2,16 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
     model() {
-        let todos = [
-            {
-                title: 'Learn Ember',
-                complete: false
-            },
-            {
-                title: 'Save World Hunger',
-                complete: false
-            }
-        ];
-        return todos;
+        this.store.findAll('todo');
     }
 });
