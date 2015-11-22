86ab4e79777ce49a052c0a97cbc8407de3dac6d0
mathiasbergqvist <mathiasbergqvist88@gmail.com>
2015-11-22 21:19:44 +0100

    added nested routes so that both routes to complete and incomplete now works

--- app/routes/todos/complete.js
+++ app/routes/todos/complete.js
@@ -1,4 +1,15 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+    model() {
+            return this.store.filter('todo', function(todo) {
+                return todo.get('complete');
+            });
+        },
+        renderTemplate(controller, model) {
+            //tells ember to render the template for todos.index (app/templates/todos/index.hbs) and apply the model for this route as the model.
+            this.render('todos.index', {
+                model: model
+            });
+        }
 });
